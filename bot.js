2f1a410bbf422cfa068016e4a3c0d90444ab8d26
101-00-0-100-0 <55638931+101-00-0-100-0@users.noreply.github.com>
2019-09-21 15:09:38 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,15 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
     if (message.content === 'ping') {
-
-       message.reply('pong');
-
-       }
-
+    	message.reply('pong');
+  	}
 });
 
- 
-
-
-
-client.login(process.env.NjI1MDcwNTg2MTkxOTM3NTc2.XYaVvA.9qC1D_eT3hqN2DCwC1fE8YK_rDM);
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NjI1MDcwNTg2MTkxOTM3NTc2.XYaVvA.9qC1D_eT3hqN2DCwC1fE8YK_rDM);//where BOT_TOKEN is the token of our bot 
